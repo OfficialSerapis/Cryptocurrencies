2bf6a6484efcf98e847d520dc09925eca0c9cc44
OfficialSerapis <143079761+OfficialSerapis@users.noreply.github.com>
2026-07-07 15:26:12 +0100

    Add files via upload

---
+++ ElectrumToken.sol
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ElectrumToken is ERC20 {
+    constructor() ERC20("Electrum", "ELC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1 million tokens to deployer
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
